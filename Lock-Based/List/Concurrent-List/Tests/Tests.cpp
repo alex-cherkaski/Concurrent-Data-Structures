c510dbc92a548354b5a4d5f87931b652c3f9c6d7
Alex Cherkaski <alexcher81@gmail.com>
2021-04-30 22:22:34 -0400

    Start concurrent list implementation.

---
+++ Lock-Based/List/Concurrent-List/Tests/Tests.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Concurrent-List/Source/ConcurrentList.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+ConcurrentList<int> g_concurrentList;
+
+namespace Tests
+{
+	TEST_CLASS(Tests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+}
